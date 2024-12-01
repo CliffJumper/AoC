fb94c8e6bfa2b1c9a3ea30a656753454372f4da9
CliffJumper <1405566+CliffJumper@users.noreply.github.com>
2024-12-01 14:24:32 -0500

    Cleaned-up types and Added HashMap example

--- 2024/rust/day01/src/main.rs
+++ 2024/rust/day01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -12,9 +13,9 @@ where
 }
 
 // For Part 1
-fn find_diff(first_list: Vec<u32>, second_list: Vec<u32>) -> u32 {
+fn find_diff(first_list: Vec<usize>, second_list: Vec<usize>) -> usize {
     let mut n = 0;
-    let mut sum: u32 = 0;
+    let mut sum = 0;
     while n < first_list.len() {
         sum += first_list[n].abs_diff(second_list[n]);
         n += 1;
@@ -23,14 +24,36 @@ fn find_diff(first_list: Vec<u32>, second_list: Vec<u32>) -> u32 {
 }
 
 // For Part 2
-fn find_similarity(first_list: Vec<u32>, second_list: Vec<u32>) -> u32 {
+fn find_similarity(first_list: Vec<usize>, second_list: Vec<usize>) -> usize {
     let mut n = 0;
     let mut similarity = 0;
 
     while n < first_list.len() {
-        similarity +=
-            first_list[n] * (second_list.iter().filter(|&i| *i == first_list[n]).count()) as u32;
+        similarity += first_list[n] * (second_list.iter().filter(|&i| *i == first_list[n]).count());
         n += 1;
+    }
+    similarity
+}
+
+// Try Part 2 using HashMap
+fn find_similarity_hash(first_list: Vec<usize>, second_list: Vec<usize>) -> usize {
+    let mut similarity = 0;
+    let mut second_count = HashMap::new();
+    for item in second_list.iter() {
+        second_count.insert(
+            item,
+            1 + if second_count.contains_key(item) {
+                second_count[item]
+            } else {
+                0
+            },
+        );
+    }
+
+    for item in first_list.iter() {
+        if second_count.contains_key(item) {
+            similarity += item * second_count[item];
+        }
     }
     similarity
 }
@@ -42,9 +65,9 @@ fn main() {
         for line in lines.flatten() {
             //    println!("{}", line);
             let mut split = line.split_whitespace();
-            let first: u32 = split.next().unwrap().parse().unwrap();
+            let first = split.next().unwrap().parse().unwrap();
             first_list.push(first);
-            let second: u32 = split.next().unwrap().parse().unwrap();
+            let second = split.next().unwrap().parse().unwrap();
             second_list.push(second);
         }
 
@@ -56,10 +79,15 @@ fn main() {
 
         println!("Part 1 Sum {}", sum);
 
-        // Part 2
+        // Part 2 -- Brute  (Iterates Vec 2 for each item in Vec 1 -- O(n^2))
         println!(
             "Part 2 Similarity {}",
-            find_similarity(first_list, second_list)
+            find_similarity(first_list.clone(), second_list.clone())
+        );
+        // Part 2 -- Use Map (Iterates each vec once -- O(2n))
+        println!(
+            "Part 2 Similarity Hash {}",
+            find_similarity_hash(first_list, second_list)
         );
     }
 }

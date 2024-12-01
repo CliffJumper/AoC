4b221df48e7580d894db08ed2f542aaca612514d
CliffJumper <1405566+CliffJumper@users.noreply.github.com>
2024-12-01 11:45:11 -0500

    Comment b/c I'll keep reusing this bit

--- 2024/rust/day01/src/main.rs
+++ 2024/rust/day01/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+// File Reading noise
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
